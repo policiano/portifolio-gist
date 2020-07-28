7e6643e7f6b7f449a90141083973a398ee7333ff
Will Policiano <w.policiano@gmail.com>
2020-07-27 22:53:18 -0300

    [#9] Create GitDigest entity (#19)
    ## Issue Link 🔗
    Create Gist Digest entity #9
    
    ## Goals ⚽
    Abstract the Gist API to protect the application from external changes
    
    ## Implementation Details 🚧
    Create a pure entity to represent a short version of a gist
    
    ## Testing Details 🔍
    Run the unit tests

---
+++ Gist/Domain/Entities/GistDigest.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+public struct GistDigest {
+    public let description: String?
+    public let owner: Owner
+    public let files: [File]
+
+    public init(description: String?, owner: Owner, files: [File]) {
+        self.description = description
+        self.owner = owner
+        self.files = files
+    }
+}
+
+extension GistDigest {
+    public struct File {
+        public let name: String
+        public let type: String
+
+        public init(name: String, type: String) {
+            self.name = name
+            self.type = type
+        }
+    }
+}
+
+extension GistDigest {
+    public struct Owner {
+        public let name: String
+        public let avatarUrl: URL?
+
+        public init(name: String, avatarUrl: URL?) {
+            self.name = name
+            self.avatarUrl = avatarUrl
+        }
+    }
+}
